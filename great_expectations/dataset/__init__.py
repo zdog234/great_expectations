d6f121d8ce8b88a479cc8f554391e45b843e0d1f
Abe <abegong@gmail.com>
2018-03-14 18:40:55 -0700

    scaffold FileSystemDataSet

--- great_expectations/dataset/__init__.py
+++ great_expectations/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .base import DataSet
 from .pandas_dataset import MetaPandasDataSet, PandasDataSet
 from .sqlalchemy_dataset import MetaSqlAlchemyDataSet, SqlAlchemyDataSet
+from .file_system_dataset import MetaFileSystemDataSet, FileSystemDataSet
