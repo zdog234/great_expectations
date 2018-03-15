fdcb1fd6092038c6f937be2794da2193b0fea87b
Abe <abegong@gmail.com>
2018-03-14 18:46:41 -0700

    Scaffold expect_file_to_be_csv_parseable

--- tests/test_file_system_dataset.py
+++ tests/test_file_system_dataset.py
@@ -13,11 +13,11 @@ import great_expectations as ge
 class TestFileSystemDataset(unittest.TestCase):
 
     def setUp(self):
-        self.filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"test_fixtures/"
-        self.D = ge.dataset.FileSystemDataSet()
+        filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"test_fixtures/"
+        self.D = ge.dataset.FileSystemDataSet(filepath)
 
     def test_expect_file_to_be_csv_parseable(self):
-        self.D.expect_file_to_be_csv_parseable("test_csv_parseable.csv", self.filepath)
+        self.D.expect_file_to_be_csv_parseable("test_csv_parseable.csv")
 
 if __name__ == "__main__":
     unittest.main()
