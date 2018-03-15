5ce673157d825a333bc0e245bb51dfce4dde1f2d
Abe <abegong@gmail.com>
2018-03-14 18:41:08 -0700

    scaffold FileSystemDataSet

--- tests/test_file_system_dataset.py
+++ tests/test_file_system_dataset.py
@@ -14,7 +14,7 @@ class TestFileSystemDataset(unittest.TestCase):
 
     def setUp(self):
         self.filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"test_fixtures/"
-        self.D = ge.dataset.FileParserDataSet()
+        self.D = ge.dataset.FileSystemDataSet()
 
     def test_expect_file_to_be_csv_parseable(self):
         self.D.expect_file_to_be_csv_parseable("test_csv_parseable.csv", self.filepath)
