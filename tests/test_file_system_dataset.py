ffcb338b5ba4d32de02c0e9e2dea28bd8ab65c81
Abe <abegong@gmail.com>
2018-03-14 18:37:53 -0700

    Scaffold first unit test

---
+++ tests/test_file_system_dataset.py
@@ -0,0 +1,23 @@
+"""
+Notes:
+* The metadata dictionary should probably
+
+"""
+
+import unittest
+import os
+import inspect
+
+import great_expectations as ge
+
+class TestFileSystemDataset(unittest.TestCase):
+
+    def setUp(self):
+        self.filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"test_fixtures/"
+        self.D = ge.dataset.FileParserDataSet()
+
+    def test_expect_file_to_be_csv_parseable(self):
+        self.D.expect_file_to_be_csv_parseable("test_csv_parseable.csv", self.filepath)
+
+if __name__ == "__main__":
+    unittest.main()
